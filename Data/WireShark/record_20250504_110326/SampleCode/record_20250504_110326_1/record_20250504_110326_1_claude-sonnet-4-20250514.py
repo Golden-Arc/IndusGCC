ddb21823ece591d0436bc72fbd59574cc736b974
Golden-Arc <1811701@mail.nankai.edu.cn>
2025-06-08 23:49:56 +0800

    UPDATE:Wireshark Dataset

---
+++ Data/WireShark/record_20250504_110326/SampleCode/record_20250504_110326_1/record_20250504_110326_1_claude-sonnet-4-20250514.py
@@ -0,0 +1,117 @@
+import pyautogui
+import time
+
+pyautogui.FAILSAFE = True
+
+pyautogui.click(x=478, y=337)
+time.sleep(1.3)
+
+pyautogui.click(x=65, y=64)
+time.sleep(0.983)
+
+pyautogui.click(x=1040, y=564)
+time.sleep(1.25)
+
+pyautogui.click(x=221, y=1057)
+time.sleep(1.417)
+
+pyautogui.click(x=594, y=28)
+time.sleep(2.267)
+
+pyautogui.click(x=736, y=18)
+time.sleep(1.383)
+
+pyautogui.click(x=150, y=252)
+time.sleep(1.983)
+
+pyautogui.click(x=1160, y=781)
+time.sleep(1.183)
+
+pyautogui.click(x=203, y=32)
+time.sleep(0.667)
+
+pyautogui.click(x=424, y=64)
+time.sleep(0.967)
+
+pyautogui.keyDown('ctrl')
+time.sleep(1.033)
+
+pyautogui.press('enter')
+time.sleep(2.533)
+
+pyautogui.click(x=535, y=1052)
+time.sleep(1.517)
+
+pyautogui.click(x=206, y=122)
+time.sleep(2.067)
+
+pyautogui.click(x=357, y=127)
+time.sleep(4.050)
+
+pyautogui.click(x=164, y=796)
+time.sleep(0.700)
+
+pyautogui.click(x=173, y=805)
+time.sleep(0.767)
+
+pyautogui.click(x=405, y=131)
+time.sleep(0.517)
+
+pyautogui.click(x=404, y=145)
+time.sleep(12.983)
+
+pyautogui.click(x=254, y=141)
+time.sleep(0.900)
+
+pyautogui.click(x=333, y=146)
+time.sleep(3.200)
+
+pyautogui.click(x=30, y=812)
+time.sleep(0.500)
+
+pyautogui.click(x=164, y=812)
+time.sleep(5.283)
+
+pyautogui.click(x=308, y=618)
+time.sleep(0.150)
+
+pyautogui.click(x=308, y=618)
+time.sleep(3.417)
+
+pyautogui.click(x=265, y=637)
+time.sleep(0.500)
+
+pyautogui.click(x=264, y=626)
+time.sleep(0.617)
+
+pyautogui.click(x=265, y=642)
+time.sleep(0.467)
+
+pyautogui.click(x=265, y=651)
+time.sleep(0.467)
+
+pyautogui.click(x=265, y=675)
+time.sleep(32.217)
+
+pyautogui.click(x=407, y=146)
+time.sleep(4.867)
+
+pyautogui.click(x=310, y=1061)
+time.sleep(2.733)
+
+pyautogui.click(x=292, y=706)
+time.sleep(1.683)
+
+pyautogui.click(x=224, y=1044)
+time.sleep(1.250)
+
+pyautogui.click(x=100, y=57)
+time.sleep(1.683)
+
+pyautogui.click(x=512, y=1069)
+time.sleep(1.067)
+
+pyautogui.click(x=294, y=182)
+time.sleep(3.650)
+
+pyautogui.click(x=173, y=648)
